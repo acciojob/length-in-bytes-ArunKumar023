5273b85ceb27c4576c8c38cfde3dfb5c3cebf780
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-24 15:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const byteSize = (str) => {
-  // write your code here
+  let totalBytes = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    // Get the character code of the current character
+    const charCode = str.charCodeAt(i);
+
+    // Count the number of bytes for this character
+    if (charCode <= 0x7f) {
+      // ASCII character (1 byte)
+      totalBytes += 1;
+    } else if (charCode <= 0x7ff) {
+      // 2-byte character
+      totalBytes += 2;
+    } else if (charCode <= 0xffff) {
+      // 3-byte character
+      totalBytes += 3;
+    } else {
+      // 4-byte character
+      totalBytes += 4;
+    }
+  }
+
+  return totalBytes;
 };
 
 // Do not change the code below
